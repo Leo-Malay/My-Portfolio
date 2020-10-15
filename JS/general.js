40863506143bd450e7febcf733a38354da99b772
Malay Bhavsar <55908198+Leo-Malay@users.noreply.github.com>
2020-10-15 20:15:53 +0530

    Add files via upload

--- JS/general.js
+++ JS/general.js
@@ -2,23 +2,13 @@
 var num = 0;
 
 function color_my_name() {
-    color_ls = [
-        "#f4f802",
-        "#02f89a",
-        "#02f0f8",
-        "#0281f8",
-        "#b41df0",
-        "#f80254",
-        "#ffffff",
-    ];
+    color_ls = ["#CC2B5E","#C22D64","#B72F69","#AC316E","#A13373","#963579","#8B377E","#863881","#803983","#753A88",
+                "#753A88","#803983","#863881","#8B377E","#963579","#A13373","#AC316E","#B72F69","#C22D64","#CC2B5E"];
     setInterval(function () {
-        document.getElementById("title").style.color = color_ls[num];
+        document.getElementById("page_head_title").style.color = color_ls[num];
         num = num + 1;
-        if (num >= 7) {
-            num = num - 7;
+        if (num >= 20) {
+            num = num - 20;
         }
-        //if (screen.width <= 1000) {
-        //    window.location.replace("./warn.html"); //path to your mobile site
-        //}
-    }, 1200);
+    }, 200);
 }
